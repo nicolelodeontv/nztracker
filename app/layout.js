3d281c1c363c8f8c02fea08f96d9600028cd4c7e
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-08-23 06:47:35 +0800

    Load reputation gain animation styles

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import './layout-overrides.css';
 import './chaos-theme.css';
 import './mobile-responsive.css';
+import './rep-pop.css';
 import MemberHistory from './member-history';
 import MemberLiveSort from './member-live-sort';
 
