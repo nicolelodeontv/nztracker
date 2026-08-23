de41e8a86628d1ed713186adec99e2c09b6ae7da
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-08-23 08:03:32 +0800

    Load sidebar cleanup styling

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import './rep-pop.css';
 import './final-theme.css';
 import './damn-good-theme.css';
 import './nav-polish.css';
+import './sidebar-cleanup.css';
 import MemberHistory from './member-history';
 import MemberLiveSort from './member-live-sort';
 import NavBehavior from './nav-behavior';
