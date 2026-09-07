1da94da3c05e5838cb3cd297debaf094130e94fb
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-07 10:44:22 +0800

    Restore shared clan war rules module

---
+++ app/clan-war-rules.js
@@ -0,0 +1,10 @@
+export {
+  CLAN_WAR_RULES,
+  getAffectedTargets,
+  getBleedingThreshold,
+  getDrainFloor,
+  getRecoveryAmount,
+  getRewardForDifference,
+  getVictoryResult,
+  isBleeding as isMemberBleeding
+} from './lib/game-rules.mjs';
