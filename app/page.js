7e2851ff6e5c631794c2d621d8b4bc0b0e26f86e
Nicole John Dela Cruz <njuandelacruz@gmail.com>
2026-09-13 16:24:39 +0800

    Use multi-source tracker dashboard as production page

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import NZTrackerDashboard from './components/NZTrackerDashboard';
+
+export const metadata = {
+  title: 'Ninja Zenshin Tracker',
+  description: 'Multi-source player, clan, PvE and PvP tracking for Ninja Zenshin.',
+};
+
+export default function Page() {
+  return <NZTrackerDashboard />;
+}
